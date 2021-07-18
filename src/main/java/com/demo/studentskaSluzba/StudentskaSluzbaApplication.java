732384720fe01e36b9754ae49041d77bbf4eceff
aleksadmitrovic <dmitrovicaleksa@gmail.com>
2021-07-18 15:01:22 +0200

    first commit

---
+++ src/main/java/com/demo/studentskaSluzba/StudentskaSluzbaApplication.java
@@ -0,0 +1,14 @@
+package com.demo.studentskaSluzba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentskaSluzbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentskaSluzbaApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/demo/studentskaSluzba/StudentskaSluzbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.studentskaSluzba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentskaSluzbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
